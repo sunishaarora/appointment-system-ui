e72b4823e15d6d1ec113e5de1d7e319f897ebd6c
Aashi Agrawal <118560049+aashiagr2022@users.noreply.github.com>
2023-07-18 13:32:11 -0500

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import backgroundImage from '../backgroundImage.jpg';
+
 
 const Homepage = () => {
   const navigate = useNavigate();
@@ -13,18 +15,23 @@ const Homepage = () => {
   };
 
   return (
-      <div className="flex flex-col items-center justify-start h-screen pt-16">
+      <div className="flex flex-col items-center justify-start h-screen pt-16" style={{backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "100vh",
+      }}>
         <h1 className="text-4xl text-center font-bold mb-8">Welcome to the Appointment Management System!</h1>
         <div className="space-x-4">
           <button
               onClick={handleUserButtonClick}
-              className="rounded text-white font-semibold bg-black hover:bg-gray-900 py-4 px-12 w-72"
+              className="rounded text-white font-semibold bg-gray-800 py-4 px-12 w-72"
           >
             Users
           </button>
           <button
               onClick={handleApptsButtonClick}
-              className="rounded text-white font-semibold bg-black hover:bg-gray-900 py-4 px-12 w-72"
+              className="rounded text-white font-semibold bg-gray-800 py-4 px-12 w-72"
           >
             Appointments
           </button>
